cf234071a0365e9a183461170b999dede339916c
user <aakashnarayanan@hotmail.com>
2020-02-13 09:56:15 +0200

    20200213_099_52

---
+++ Variables, expressions, and statements.py
@@ -0,0 +1,183 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> type('Hello, World!')
+<class 'str'>
+>>> type(17)
+<class 'int'>
+>>> type(5.3)
+<class 'float'>
+>>> type('15')
+<class 'str'>
+>>> type('5.6')
+<class 'str'>
+>>> print(1,000,000)
+1 0 0
+>>> primnt
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    primnt
+NameError: name 'primnt' is not defined
+>>> print(000,111,111)
+0 111 111
+>>> message = 'and now for something completely different'
+>>> n = 17
+>>> pi= 3.1415985323
+>>> print(n)
+17
+>>> print(pi)
+3.1415985323
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 23rombas = 'big day'
+SyntaxError: invalid syntax
+>>> morewe@ =25000
+SyntaxError: invalid syntax
+>>> class = Advance Theoretical
+SyntaxError: invalid syntax
+>>> class = Advance theoretical
+SyntaxError: invalid syntax
+>>> print(1)
+1
+>>> x = 2
+>>> print(x)
+2
+>>> minute = 59
+>>> minute/60
+0.9833333333333333
+>>> minute ==e underscore character ( _ ) can appear in a name. It is often used in names with multiple words, such as my_name or airspeed_of_unladen_swallow. Variable names can start with an underscore character, but we generally avoid doing this unless we are writing library code for others to
+SyntaxError: invalid syntax
+>>> minute = 59
+>>> minute//60
+0
+>>> 17
+17
+>>> X
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    X
+NameError: name 'X' is not defined
+>>> x
+2
+>>> x + 17
+19
+>>> x
+2
+>>> 1 + 6
+7
+>>> x = 5
+>>> x + 17
+22
+>>> 2 * (3-1)
+4
+>>> (1+1)**(5-2)
+8
+>>> 2**1+1
+3
+>>> 3*1**3
+3
+>>> 2*3-1
+5
+>>> 2**3-1
+7
+>>> 5**1-2
+3
+>>> 1**5-1
+0
+>>> 6+4/2
+8.0
+>>> 2*3-1
+5
+>>> 5-3-1
+1
+>>> 1-3-5
+-7
+>>> quotient = 7//3
+>>> print(quotient)
+2
+>>> remainder = 7//3
+>>> print(remainder)
+2
+>>> remainder = 7%3
+>>> print(remainder)
+1
+>>> first = 25
+>>> second = 10
+>>> print( first+second)
+35
+>>> first=25
+>>> second='10'
+>>> print(first+second)
+Traceback (most recent call last):
+  File "<pyshell#60>", line 1, in <module>
+    print(first+second)
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> first='25'
+>>> second='10'
+>>> print(first+second)
+2510
+>>> first = 'TESTING'
+>>> second = 3
+>>> print(first*second)
+TESTINGTESTINGTESTING
+>>> first = 'TESTING'
+>>> second= 4
+>>> print(first * second)
+TESTINGTESTINGTESTINGTESTING
+>>> inp = input()
+welcome to RTU
+>>> print(inp)
+welcome to RTU
+>>> name = input('What is your name?\n')
+What is your name?
+mahadevan
+>>> print(name)
+mahadevan
+>>> inp = input('what is your name?\n')
+what is your name?
+mahadevan
+>>> print(name)
+mahadevan
+>>> print(inp)
+mahadevan
+>>> prompt = 'What...is the airspeed velocity of an unladen swallow?\n'
+>>> speed = input(prompt)
+What...is the airspeed velocity of an unladen swallow?
+12
+>>> int(speed)
+12
+>>> int(speed)+6
+18
+>>> v=5 #assign 5 to v
+>>> v = 6 #velocity in meters/seconds
+>>> 
+>>> 
+>>> #Choosing mnemonic variable names
+>>> a=35.0
+>>> b=12.50
+>>> c=a * b
+>>> print(c)
+437.5
+>>> 
+>>> hours = 25.0
+>>> rate = 12.50
+>>> pay = hours * rate
+>>> print(pay)
+312.5
+>>> x1q3z9ahd = 35.0
+>>> x1q3z9afd = 12.50
+
+>>> x1q3p9afd = x1q3z9ahd * x1q3z9afd
+>>> print(x1q3p9afd)
+437.5
+>>> principal = 327.68
+>>> intrest = principal * rate
+>>> rate = 20000
+>>> intrest = principal * rate
+>>> 
